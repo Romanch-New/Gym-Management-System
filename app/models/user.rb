0665230d15081b418342175d8bdd94536b370cc6
Romanch Sharma <romanchsharma@icloud.com>
2024-01-18 13:44:34 +0530

    complete user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6, maximum: 128 }
+  validates :password_confirmation, presence: true
+  validates :admin, inclusion: { in: [true, false] }
 end
