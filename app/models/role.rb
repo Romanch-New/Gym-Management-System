06be18abe48c464ea69e865dcc1ee8898156127c
Romanch Sharma <romanchsharma@icloud.com>
2024-01-18 14:00:01 +0530

    initialize rolify and role test

---
+++ app/models/role.rb
@@ -0,0 +1,30 @@
+class Role < ApplicationRecord
+  PREDEFINED_ROLES = %w[admin new_user member staff coach nutritionist guest].freeze
+  has_and_belongs_to_many :users, join_table: "users_roles"
+  
+  belongs_to :resource,
+             :polymorphic => true,
+             :optional => true
+  
+
+  validates :resource_type,
+            :inclusion => { :in => Rolify.resource_types },
+            :allow_nil => true
+
+
+  validates_each :name do |record, attr, value|
+    unless PREDEFINED_ROLES.include?(value)
+      record.errors.add(attr, 'is not a predefined role')
+    end
+  end
+
+  after_initialize :set_default_role, if: :new_record?
+
+  scopify
+
+  private
+
+  def set_default_role
+    self.name ||= 'guest'
+  end
+end
---
+++ test/models/role_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class RoleTest < ActiveSupport::TestCase
+  test "role with admin name can be created" do
+    role = Role.new(name: 'admin')
+    assert role.valid?
+  end
+  test "role with new_user name can be created" do
+    role = Role.new(name: 'new_user')
+    assert role.valid?
+  end
+  test "role with coach name can be created" do
+    role = Role.new(name: 'coach')
+    assert role.valid?
+  end
+  test "role with nutritionist name can be created" do
+    role = Role.new(name: 'nutritionist')
+    assert role.valid?
+  end
+  test "role with member name can be created" do
+    role = Role.new(name: 'member')
+    assert role.valid?
+  end
+  test "role with staff name can be created" do
+    role = Role.new(name: 'staff')
+    assert role.valid?
+  end
+  test "role with guest name can be created" do
+    role = Role.new(name: 'guest')
+    assert role.valid?
+  end
+
+  test "role with non-predefined name cannot be created" do
+    role = Role.new(name: 'non_predefined_role')
+    assert_not role.valid?
+    assert_equal ["is not a predefined role"], role.errors[:name]
+  end
+end
