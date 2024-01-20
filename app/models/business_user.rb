eb65a5c784dc752b00f202bfb7559cdb861b1d76
Romanch Sharma <romanchsharma@icloud.com>
2024-01-20 06:00:11 +0530

    fix all issues with Rubocop and rubocop rails, changed has_and_belong_to_many to has_many through association

--- app/models/business_user.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class BusinessUser < ActiveRecord::Base
-  belongs_to :business
-  belongs_to :user
-  validates :business_id, presence: true
-  validates :user_id, presence: true
-  validates :role, presence: true
-end
