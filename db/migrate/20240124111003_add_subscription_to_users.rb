c21f668f6423839872b4bf943d535e1ea81608ed
Romanch Sharma <romanchsharma@icloud.com>
2024-01-24 17:32:52 +0530

    add subscription status to user table and updated schema, users.yml

--- db/migrate/20240124111003_add_subscription_to_users.rb
+++ db/migrate/20240124111003_add_subscription_to_users.rb
@@ -1,5 +1,6 @@
 class AddSubscriptionToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :subscription, :boolean
+    add_column :users, :subscription,
+               :boolean, default: false, null: false
   end
 end
